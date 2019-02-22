bb652fb79544ca6720010277024a7006338492b1
Mydekaifa <1012737795@qq.com>
2019-02-22 11:02:47 +0800

    项目准备

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//基础样式
+import './assets/styles/reset.css'
+//1像素边框解决高倍屏，边框1像素不同问题
+import './assets/styles/border.css'
+//解决移动端click事件延迟300ms  通过npm安装依赖
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+//attach是fastClick的一个方法，绑定在Body身上
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,12 @@
 <template>
-  <div id="app">
-    hello
-  </div>
+  <div>home</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
 <style>
-
 </style>
