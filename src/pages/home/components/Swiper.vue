03f721e3d12c6e3d576eadb5a5010b7ad8967d8f
Mydekaifa <1012737795@qq.com>
2019-02-25 11:18:58 +0800

    完成轮播图，解决网络缓慢加载图片市，底部内容会抖动的问题。

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,17 +1,8 @@
 <template>
-    <div>
-        <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
-            <swiper-slide>
-               <img src="../../../../static/imgs/home_banner_1.jpg" />
-            </swiper-slide>
-            <swiper-slide>
-               <img src="../../../../static/imgs/home_banner_2.jpg" />
-            </swiper-slide>
-            <swiper-slide>
-               <img src="../../../../static/imgs/home_banner_3.jpg" />
-            </swiper-slide>
-            <swiper-slide>
-               <img src="../../../../static/imgs/home_banner_4.jpg" />
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide  v-for="items of swiperList" :key='items.id'>
+               <img :src="items.imgurl" />
             </swiper-slide>
             <div class="swiper-pagination"  slot="pagination"></div>
         </swiper>
@@ -23,12 +14,35 @@ export default {
   name: 'HomeSwiper',
   data () {
     return {
-      swiperOption: {}
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {id: '0001', imgurl: '../../../../static/imgs/home_banner_1.jpg'},
+        {id: '0002', imgurl: '../../../../static/imgs/home_banner_2.jpg'},
+        {id: '0003', imgurl: '../../../../static/imgs/home_banner_3.jpg'},
+        {id: '0004', imgurl: '../../../../static/imgs/home_banner_4.jpg'}
+      ]
     }
   }
 }
 </script>
 
 <style scoped>
-
+.wrapper{
+overflow: hidden;
+width: 100%;
+height: 0;
+padding-bottom: 25.6%;
+background: #eee;
+                                       /********** 宽高比自适应,防止图片没加载时,图片下方的内容移到上方 **********/
+}
+.wrapper>>>.swiper-pagination-bullet-active{
+background: #FFFFFF;
+/* ">>>"表示样式穿透,让.wrapper下所有的.swiper-pagination-bullet-active,不被scoped限制 */
+}
+img{
+width: 100%;
+}
 </style>
