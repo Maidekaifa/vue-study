4be0877c490773f4f423276536ece7fd9b1f471b
mai <1012737795@qq.com>
2019-05-28 21:25:27 +0800

    完成图片预览效果

--- src/pages/detail/components/banner.vue
+++ src/pages/detail/components/banner.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="banner">
-    <img src="../../../../static/imgs/banner4.jpg" />
-    <div class="banner_info">
-      <div class="banner_title">海南三亚(AAAA景区)</div>
-      <div class="banner_number">
-        <span class="iconfont banner_icon">&#xe670;</span>5
+  <div>
+    <div class="banner" @click="show_commongallary">
+      <img src="../../../../static/imgs/banner4.jpg" />
+      <div class="banner_info">
+        <div class="banner_title">海南三亚(AAAA景区)</div>
+        <div class="banner_number">
+          <span class="iconfont banner_icon">&#xe670;</span>5
+        </div>
       </div>
     </div>
+    <common-gallary :imgs="imgs" v-show="commongallary_show" @hidden="close_commongallary"></common-gallary>
   </div>
 </template>
 
 <script>
+import CommonGallary from 'common/gallary/Gallary'
 export default {
-  name: 'DetailBanner'
+  name: 'DetailBanner',
+  data () {
+    return {
+      commongallary_show: false,
+      imgs: ['../../../../static/imgs/banner1.jpg', '../../../../static/imgs/banner2.jpg', '../../../../static/imgs/banner3.jpg', '../../../../static/imgs/banner4.jpg', '../../../../static/imgs/banner5.jpg']
+    }
+  },
+  components: {
+    CommonGallary
+  },
+  methods: {
+    show_commongallary () {
+      this.commongallary_show = true
+    },
+    close_commongallary () {
+      this.commongallary_show = false
+    }
+  }
 }
 </script>
 
